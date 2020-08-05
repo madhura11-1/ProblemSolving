50056d0d69ff59f8016473740544ede3ead90e64
madhura11-1 <madhurakunjir2611@gmail.com>
2020-08-05 20:42:48 +0530

    Truck tour

--- Cpp/Trucktour.cpp
+++ Cpp/Trucktour.cpp
@@ -3,32 +3,36 @@ using namespace std;
 
 
 int truckTour(vector<vector<int>> petrolpumps) {
-  
-  int n = petrolpumps.size();
-  int i =0;
-  int j;
-  long instant;
-  long k =0;
-  map<int,bool> mapi;
-  bool x = true;
-  while(x){
-      i = i%n;
-      instant = petrolpumps[i][0] + k;
-      if(instant >= petrolpumps[i][1]){
-          k = instant - petrolpumps[i][1];                // This solution takes way more time to find and also fails in some tests
-          mapi[i] = true;                                 // as u r using circular queue it does not guarantee to have o(n) time
-      }                                                    //also o(n) extra space
+   int sum = 0;
+   int diff = 0;
+   int j;
+   for(int i=0;i<petrolpumps.size();i++){
+      if(petrolpumps[i][0] + sum >= petrolpumps[i][1]){
+          sum = sum + (petrolpumps[i][0] - petrolpumps[i][1]);
+      }
       else{
-          mapi.clear();
+          diff += petrolpumps[i][0] - petrolpumps[i][1];
+          sum = 0;
+          j = i+1;
       }
-      i++;
-      j = i%n;
-      if(mapi.find(j) != mapi.end() && mapi[j] == true){
-          x = false;
-      }
-  }
+   }
    return j;
 }
+
+/*
+int truckTour(vector<vector<int>> petrolpumps) {        both are valid solutions
+   int sum = 0;
+   int j;
+   for(int i=0;i<petrolpumps.size();i++){
+      sum = sum + (petrolpumps[i][0] - petrolpumps[i][1]);
+      if(sum < 0){
+          sum = 0;
+          j = i + 1;
+      }
+   }
+   return j;
+}
+*/
 
 int main(){
    
