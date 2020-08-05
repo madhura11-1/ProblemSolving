e38ed5bbeee0102a81f03ca7c2e77ee648e0ea55
madhura11-1 <madhurakunjir2611@gmail.com>
2020-08-05 20:43:39 +0530

    Duplicates inside an array

---
+++ Cpp/Find_duplicate_in_array.cpp
@@ -0,0 +1,38 @@
+// detect duplicate number in an aray of size n+1 where all elemnts lie from 1 to n;
+
+//here u can apply traditional method like sort the array and find i and i+1 equal
+//  complexity : time = O(nlogn) (merge sort) space = O(1)
+
+// also u can use another array like a check array and go on marking 1 into that repective index
+// time : O(n) space O(n)
+
+
+// tis solution takes time : O(n) space ; O[1)
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void findDuplicate(int *arr){
+
+    int slow =arr[0];
+    int fast = arr[0];
+    do{
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    }while(arr[slow] != arr[fast]);
+
+    fast = arr[0];
+    while(arr[fast] != arr[slow]){
+        slow = arr[slow];
+        fast = arr[fast];
+    }
+
+    cout<<arr[slow];
+
+}
+int main(){
+
+    int arr[10] = {2,5,9,6,9,3,8,9,7,1};
+    findDuplicate(arr);
+    return 0;
+}
