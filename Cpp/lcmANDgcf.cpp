7248d01c06b0e70a097bbf872a36b91ef805d34d
madhura11-1 <madhurakunjir2611@gmail.com>
2020-09-10 07:00:22 +0530

    Day 10

---
+++ Cpp/lcmANDgcf.cpp
@@ -0,0 +1,32 @@
+// LCM = a*b/gcd(a,b); 
+// so apparently for the array take two numbers at once inlucding one number as lcm of previous two
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a,int b){
+    if(b == 0)
+    return a;
+    return gcd(b,a%b);
+}
+
+void calculateLCM(int *arr,int n){
+    int ans = arr[0];
+    for(int i= 1;i<n;i++){
+        ans = (arr[i]*ans)/gcd(arr[i],ans);
+    }
+    cout<<ans;
+}
+
+int main(){
+
+    int n;
+    cout<<"Enter number of elements to calculate the lcm of"<<endl;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    calculateLCM(arr,n);
+    return 0;
+}
