28f99a23564714621a49487288070dd8046541af
madhura11-1 <madhurakunjir2611@gmail.com>
2020-09-10 11:08:19 +0530

    Day 10

--- Cpp/lcmANDgcf.cpp
+++ Cpp/lcmANDgcf.cpp
@@ -1,6 +1,10 @@
 // LCM = a*b/gcd(a,b); 
 // so apparently for the array take two numbers at once inlucding one number as lcm of previous two
 
+// LCM wihtout gcd is where u apply brute force where in starting with 2 as common factor till the maximum number in array
+// you check if numbers in array are divisible by it and then if yes reduce the numbers and multiple ans with 2 or whatever factor.
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
